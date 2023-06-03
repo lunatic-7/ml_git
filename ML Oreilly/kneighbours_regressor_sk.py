0a8fe304071bc106956da37ca8141496be3050b6
Wasif Nadeem <wasif1607@gmail.com>
2023-06-03 18:08:06 +0530

    ML Oreilly chp 1

--- ML Oreilly/kneighbours_regressor_sk.py
+++ ML Oreilly/kneighbours_regressor_sk.py
@@ -17,7 +17,8 @@ plt.axis([23_500,62_500,4,9])  # x_start=23_500, x_end=62_500, y_start=4, y_end=
 plt.show()
 
 # Select a KNeighbourRegressor model
-# (Ye pooche hue X_new ke aas pss ke 3 values dkhega aur unka average output mai dega)
+# (Ye pooche hue X_new ke aas pss ke 3 outputs ko dekhega aur unka average output mai dega)
+# eg: here, (5.9 + 5.9 + 7.2) / 3 == 6.3333 
 model = KNeighborsRegressor(n_neighbors=3)
 
 # Train the model
@@ -25,4 +26,36 @@ model.fit(X, Y)
 
 # Make a prediction for Cyprus
 X_new = [[37_655.2]]  # Cyprus GDT per capita in 2020
-print(model.predict(X_new))  # Output: [[6.30165767]]
+print(model.predict(X_new))  # Output: [[6.33333333]]
+
+
+########## TRAINING DATA ###########
+
+#            Country  GDP per capita (USD)  Life satisfaction
+# 0           Russia          26456.387938                5.8
+# 1           Greece          27287.083401                5.4
+# 2           Turkey          28384.987785                5.5
+# 3           Latvia          29932.493910                5.9
+# 4          Hungary          31007.768407                5.6
+# 5         Portugal          32181.154537                5.4
+# 6           Poland          32238.157259                6.1
+# 7          Estonia          35638.421351                5.7
+# 8            Spain          36215.447591                6.3
+# 9         Slovenia          36547.738956                5.9
+# 10       Lithuania          36732.034744                5.9
+# 11          Israel          38341.307570                7.2
+# 12           Italy          38992.148381                6.0
+# 13  United Kingdom          41627.129269                6.8
+# 14          France          42025.617373                6.5
+# 15     New Zealand          42404.393738                7.3
+# 16          Canada          45856.625626                7.4
+# 17         Finland          47260.800458                7.6
+# 18         Belgium          48210.033111                6.9
+# 19       Australia          48697.837028                7.3
+# 20          Sweden          50683.323510                7.3
+# 21         Germany          50922.358023                7.0
+# 22         Austria          51935.603862                7.1
+# 23         Iceland          52279.728851                7.5
+# 24     Netherlands          54209.563836                7.4
+# 25         Denmark          55938.212809                7.6
+# 26   United States          60235.728492                6.9
--- ML Oreilly/linear_regression_sk.py
+++ ML Oreilly/linear_regression_sk.py
@@ -25,3 +25,35 @@ model.fit(X, Y)
 # Make a prediction for Cyprus
 X_new = [[37_655.2]]  # Cyprus GDT per capita in 2020
 print(model.predict(X_new))  # Output: [[6.30165767]]
+
+
+########## TRAINING DATA ###########
+
+#            Country  GDP per capita (USD)  Life satisfaction
+# 0           Russia          26456.387938                5.8
+# 1           Greece          27287.083401                5.4
+# 2           Turkey          28384.987785                5.5
+# 3           Latvia          29932.493910                5.9
+# 4          Hungary          31007.768407                5.6
+# 5         Portugal          32181.154537                5.4
+# 6           Poland          32238.157259                6.1
+# 7          Estonia          35638.421351                5.7
+# 8            Spain          36215.447591                6.3
+# 9         Slovenia          36547.738956                5.9
+# 10       Lithuania          36732.034744                5.9
+# 11          Israel          38341.307570                7.2
+# 12           Italy          38992.148381                6.0
+# 13  United Kingdom          41627.129269                6.8
+# 14          France          42025.617373                6.5
+# 15     New Zealand          42404.393738                7.3
+# 16          Canada          45856.625626                7.4
+# 17         Finland          47260.800458                7.6
+# 18         Belgium          48210.033111                6.9
+# 19       Australia          48697.837028                7.3
+# 20          Sweden          50683.323510                7.3
+# 21         Germany          50922.358023                7.0
+# 22         Austria          51935.603862                7.1
+# 23         Iceland          52279.728851                7.5
+# 24     Netherlands          54209.563836                7.4
+# 25         Denmark          55938.212809                7.6
+# 26   United States          60235.728492                6.9
